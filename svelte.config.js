1bf421d0675e8055d9c7b20f80c1bad216b046a0
Stefan de Graaf <stefan.de.graaf@geodan.nl>
2023-10-16 13:59:06 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : "/github-pages-test2/",
+			base: dev ? '' : "/github-pages-test2/src",
 		}
 	}
 };
